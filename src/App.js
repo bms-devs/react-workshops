e4706d446f9974c7f27a085da3e2f1117e3f6e6f
Kamil Rafałko <kamil.rafalko@bms.com.pl>
2017-10-23 09:23:04 +0200

    Krok 3: Dwa przyciski

--- src/App.js
+++ src/App.js
@@ -21,9 +21,19 @@ class App extends Component {
         return this.state.index + 1
     }
 
+    getPreviousIndex() {
+        return this.state.index - 1
+    }
+
     handleNextClick() {
         this.setState({
             index: this.getNextIndex()
+        })
+    }
+
+    handlePreviousClick() {
+        this.setState({
+            index: this.getPreviousIndex()
         })
     }
 
@@ -31,6 +41,8 @@ class App extends Component {
         return (
             <div className="App">
                 <div className="viewer" style={{backgroundColor: this.colors[this.state.index]}} />
+                <button disabled={this.getPreviousIndex() < 0}
+                        onClick={() => this.handlePreviousClick()}>Previous</button>
                 <button disabled={this.getNextIndex() >= this.colors.length}
                         onClick={() => this.handleNextClick()}>Next</button>
             </div>
