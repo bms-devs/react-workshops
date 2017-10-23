05568abd0922c70f84432559af2729fe5d1c296b
Kamil Rafałko <kamil.rafalko@bms.com.pl>
2017-10-23 08:48:42 +0200

    Krok 1: Kolorowy div + przycisk zmieniający kolor na następny z listy

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor() {
+        super()
+        this.colors = [
+            "red",
+            "green",
+            "blue",
+            "black",
+            "yellow"
+        ]
+        this.state = {
+            index: 0
+        }
+    }
+
+    handleNextClick() {
+        this.setState({
+            index: this.state.index + 1
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="viewer" style={{backgroundColor: this.colors[this.state.index]}} />
+                <button onClick={() => this.handleNextClick()}>Next</button>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
