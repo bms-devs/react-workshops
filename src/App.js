df622eec989a7bc3e79038ee6dbb043765735a1b
Kamil Rafałko <kamil.rafalko@bms.com.pl>
2017-10-24 12:35:48 +0200

    Krok 1: Przycisk i div z kolorem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.colors = [
+        "red",
+        "blue",
+        "yellow",
+        "green"
+    ];
+    this.state = {
+      index: 0
+    }
+  }
+
+  handleNextClick() {
+    this.setState({
+        index: this.state.index + 1
+    })
+  }
+
   render() {
     return (
       <div className="app">
-        <h1>Hello world!</h1>
+        <div className="viewer" style={{ backgroundColor: this.colors[this.state.index] }} />
+        <button onClick={() => this.handleNextClick()}
+                disabled={(this.state.index + 1) === this.colors.length} >
+          Następny
+        </button>
       </div>
     );
   }
